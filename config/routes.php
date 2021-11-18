18aaa011ff7ebb6afed90088293992c63945e968
0neVision <70612633+0neVision@users.noreply.github.com>
2021-11-19 02:06:55 +1100

    set base path to Lanes index

--- config/routes.php
+++ config/routes.php
@@ -50,7 +50,7 @@ return static function (RouteBuilder $routes) {
          * its action called 'display', and we pass a param to select the view file
          * to use (in this case, templates/Pages/home.php)...
          */
-        $builder->connect('/', ['controller' => 'Pages', 'action' => 'display', 'home']);
+        $builder->connect('/', ['controller' => 'Lanes', 'action' => 'index']);
 
         /*
          * ...and connect the rest of 'Pages' controller's URLs.
