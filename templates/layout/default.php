1b3b96c930b9c399ea0cd39cff2b54b869ff886e
0neVision <70612633+0neVision@users.noreply.github.com>
2021-11-20 13:30:20 +1100

    initial default layout with css and js imports

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -13,45 +13,46 @@
  * @license       https://opensource.org/licenses/mit-license.php MIT License
  * @var \App\View\AppView $this
  */
-
-$cakeDescription = 'CakePHP: the rapid development php framework';
 ?>
 <!DOCTYPE html>
 <html>
 <head>
     <?= $this->Html->charset() ?>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?= $this->fetch('meta') ?>
+    <?= $this->Html->meta('icon') ?>
     <title>
-        <?= $cakeDescription ?>:
         <?= $this->fetch('title') ?>
     </title>
-    <?= $this->Html->meta('icon') ?>
 
-    <link href="https://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet">
 
-    <?= $this->Html->css(['normalize.min', 'milligram.min', 'cake']) ?>
-
-    <?= $this->fetch('meta') ?>
+    <!-- Bootstrap v5 Styles -->
+    <?= $this->Html->css(['bootstrap/bootstrap.css']) ?>
     <?= $this->fetch('css') ?>
-    <?= $this->fetch('script') ?>
+
 </head>
 <body>
-    <nav class="top-nav">
-        <div class="top-nav-title">
-            <a href="<?= $this->Url->build('/') ?>"><span>Cake</span>PHP</a>
-        </div>
-        <div class="top-nav-links">
-            <a target="_blank" rel="noopener" href="https://book.cakephp.org/4/">Documentation</a>
-            <a target="_blank" rel="noopener" href="https://api.cakephp.org/">API</a>
-        </div>
-    </nav>
-    <main class="main">
-        <div class="container">
-            <?= $this->Flash->render() ?>
+
+    <main>
+        <div class="container py-4">
             <?= $this->fetch('content') ?>
         </div>
     </main>
+
     <footer>
     </footer>
+
+    <?= $this->Flash->render() ?>
+
+    <!-- Bootstrap core JavaScript-->
+    <?= $this->Html->script('/vendor/jquery/jquery-3.6.0.min.js') ?>
+    <?= $this->Html->script('/js/bootstrap/bootstrap.min.js') ?>
+
+    <?= $this->fetch('script') ?>
 </body>
+
+
 </html>
