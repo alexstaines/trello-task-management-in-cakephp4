8708a71d5eba9ad4a05cbca9107b45b8faf84eb9
0neVision <70612633+0neVision@users.noreply.github.com>
2021-11-23 01:31:17 +1100

    added initial Lanes and Cards display with horizontal scroll - added '+ Add card' to each lane which creates an empty Card

--- templates/Lanes/index.php
+++ templates/Lanes/index.php
@@ -1,50 +1,40 @@
-<?php
-/**
- * @var \App\View\AppView $this
- * @var \App\Model\Entity\Lane[]|\Cake\Collection\CollectionInterface $lanes
- */
-?>
-<div class="lanes index content">
-    <?= $this->Html->link(__('New Lane'), ['action' => 'add'], ['class' => 'button float-right']) ?>
-    <h3><?= __('Lanes') ?></h3>
-    <div class="table-responsive">
-        <table>
-            <thead>
-                <tr>
-                    <th><?= $this->Paginator->sort('id') ?></th>
-                    <th><?= $this->Paginator->sort('position') ?></th>
-                    <th><?= $this->Paginator->sort('name') ?></th>
-                    <th><?= $this->Paginator->sort('created') ?></th>
-                    <th><?= $this->Paginator->sort('modified') ?></th>
-                    <th class="actions"><?= __('Actions') ?></th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach ($lanes as $lane): ?>
-                <tr>
-                    <td><?= $this->Number->format($lane->id) ?></td>
-                    <td><?= $this->Number->format($lane->position) ?></td>
-                    <td><?= h($lane->name) ?></td>
-                    <td><?= h($lane->created) ?></td>
-                    <td><?= h($lane->modified) ?></td>
-                    <td class="actions">
-                        <?= $this->Html->link(__('View'), ['action' => 'view', $lane->id]) ?>
-                        <?= $this->Html->link(__('Edit'), ['action' => 'edit', $lane->id]) ?>
-                        <?= $this->Form->postLink(__('Delete'), ['action' => 'delete', $lane->id], ['confirm' => __('Are you sure you want to delete # {0}?', $lane->id)]) ?>
-                    </td>
-                </tr>
-                <?php endforeach; ?>
-            </tbody>
-        </table>
-    </div>
-    <div class="paginator">
-        <ul class="pagination">
-            <?= $this->Paginator->first('<< ' . __('first')) ?>
-            <?= $this->Paginator->prev('< ' . __('previous')) ?>
-            <?= $this->Paginator->numbers() ?>
-            <?= $this->Paginator->next(__('next') . ' >') ?>
-            <?= $this->Paginator->last(__('last') . ' >>') ?>
-        </ul>
-        <p><?= $this->Paginator->counter(__('Page {{page}} of {{pages}}, showing {{current}} record(s) out of {{count}} total')) ?></p>
-    </div>
+
+
+<div class="row-lane row">
+    <?php foreach ($lanes as $lane): ?>
+        <div class="lane">
+            <div class="card bg-light shadow border-0">
+                <div class="card-header bg-light border-bottom-0"><h6 style="font-family: Arial, Helvetica, sans-serif"><?= $lane->name ?><h6></div>
+                
+                <div class="card-body bg-light p-2">
+                    <?php foreach ($lane->cards as $card): ?>
+                        <div class="card shadow border-0 m-1 mt-2">
+                            <div class="card-header bg-white border-bottom-0">
+                                <div class="row">
+                                    <div class="col-sm">
+                                        <?= $card->name ?>
+                                    </div>
+                                    <div class="col-sm">
+                                        <a href="" class="float-end"><i class="fas fa-edit text-secondary"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card-body">
+                            </div>
+                            <div class="card-footer"></div>
+                            <a class="stretched-link" href="<?= $this->Url->build(['controller' => 'Cards', 'action' => 'view', $card->id]) ?>"></a>
+                        </div>
+                    <?php endforeach; ?>
+                    <?= $this->Form->create(null, ['url' => ['controller' => 'Cards', 'action' => 'add', $lane->id]]); ?>
+                    <div class="d-grid gap-2 mt-4">
+                        <button type="submit" class="btn btn-outline-dark">+ Add card</button>
+                    </div>
+                    <?= $this->Form->end() ?>
+                </div>
+            </div>
+        </div>
+    <?php endforeach; ?>
+
 </div>
+
+
