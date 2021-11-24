f31e1f0350bb91be576ab845378e32151a598811
0neVision <70612633+0neVision@users.noreply.github.com>
2021-11-25 02:26:09 +1100

    retrieved cards now contain checklists and associated checklistItems

--- src/Controller/CardsController.php
+++ src/Controller/CardsController.php
@@ -30,7 +30,7 @@ class CardsController extends AppController
             if ($this->request->getData()['action'] == 'get') {
                 $id = intval($this->request->getData()['id']);
 
-                $card = $this->Cards->get($id);
+                $card = $this->Cards->get($id, ['contain' => ['Checklists.ChecklistItems']]);
 
                 // $task->due_date = $task->due_date->format('d/m/Y');
                 // $task->created_date = $task->created_date->format('d/m/Y h:ia');
